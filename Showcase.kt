54a5410df8f22b9742df6d2550d0147872c39cbc
tomeeeS <tamas.sajti.dev@gmail.com>
2020-01-10 13:06:38 +0100

    add fragment_place_order.xml

--- Showcase.kt
+++ Showcase.kt
@@ -1,10 +1,11 @@
 /* This is a fragment from one of my apps, and some of its dependencies.
- I believe it shows my usage of: 
+ I believe it shows my knowledge and usage  of: 
     - clean code
     - SOLID principles
-    - some (relatively) new and powerful Kotlin & Android features (from Jetpack & Architecture components)
+    - sw architectures (currently using MVVM(I))
+    - some (relatively) new and powerful Kotlin & Android features (from Jetpack & Architecture components, Kotlin coroutines)
 
- It shows a pdf on the UI, and with a button click it can open an
+ PlaceOrderFragment shows a pdf on the UI, and with a button click it can open an
   email client app with an intent, and have the pdf automatically attached and some
   information like the client's email address prefilled */
 
@@ -124,3 +125,62 @@ class PlaceOrderViewModel: PdfViewModel() {
     }
 
 }
+
+// fragment_place_order.xml:
+<?xml version="1.0" encoding="utf-8"?>
+<layout xmlns:android="http://schemas.android.com/apk/res/android"
+    xmlns:app="http://schemas.android.com/apk/res-auto">
+
+    <androidx.constraintlayout.widget.ConstraintLayout
+        android:layout_width="match_parent"
+        android:layout_height="match_parent"
+        android:orientation="horizontal"
+        >
+
+        <com.github.barteksc.pdfviewer.PDFView
+            android:id="@+id/pdf"
+            android:layout_width="0dp"
+            android:layout_height="0dp"
+            android:background="@color/background"
+
+            app:layout_constraintTop_toTopOf="parent"
+            app:layout_constraintStart_toStartOf="parent"
+            app:layout_constraintEnd_toEndOf="parent"
+            app:layout_constraintBottom_toTopOf="@+id/cancel_button"
+            >
+        </com.github.barteksc.pdfviewer.PDFView>
+
+        <hu.sajti.pevdicalculator.view.SquareImageButton
+            android:id="@+id/cancel_button"
+            android:layout_width="0dp"
+            android:layout_height="wrap_content"
+            android:layout_gravity="right"
+            android:background="@drawable/red_cancel"
+            android:scaleType="fitXY"
+            android:layout_margin="8dp"
+
+            app:layout_constraintWidth_percent="@dimen/button_width_ratio"
+
+            app:layout_constraintStart_toStartOf="parent"
+            app:layout_constraintBottom_toBottomOf="parent"
+            />
+
+        <hu.sajti.pevdicalculator.view.SquareImageButton
+            android:id="@+id/ok_button"
+            android:layout_width="0dp"
+            android:layout_height="wrap_content"
+            android:layout_gravity="right"
+            android:background="@drawable/arrow_blue_right"
+            android:scaleType="fitXY"
+            android:layout_margin="8dp"
+            isEnabled="false"
+
+            app:layout_constraintWidth_percent="@dimen/button_width_ratio"
+
+            app:layout_constraintEnd_toEndOf="parent"
+            app:layout_constraintBottom_toBottomOf="parent"
+            />
+
+    </androidx.constraintlayout.widget.ConstraintLayout>
+
+</layout>
